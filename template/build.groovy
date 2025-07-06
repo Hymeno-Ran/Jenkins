487f19fadabe9eef8de214e17003e047323be53d
Cat Lazy <catlazy@Cats-MacBook-Pro.local>
2025-07-06 20:15:14 +0700

    update jenkins template engine

--- template/build.groovy
+++ template/build.groovy
@@ -1,8 +1,8 @@
 // templates/build.groovy
-def call(String projectName) {
+def call(String pathApp) {
     stage('Build') {
         echo "Building project: ${pathApp}"
         // Thêm lệnh build ở đây, ví dụ:
-        sh "'javac -d target ${pathApp}'"
+        sh "javac -d target ${pathApp}"
     }
 }
